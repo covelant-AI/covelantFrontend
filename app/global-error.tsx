dfb2d94397fef323f93c1570060286e15a44f61b
AmNat789 <amit.amanna@code.berlin>
2025-07-26 00:39:33 +0200

    Remove Html from page

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -9,16 +9,5 @@ export default function GlobalError({ error }: { error: Error & { digest?: strin
     Sentry.captureException(error);
   }, [error]);
 
-  
-  return (
-    <html>
-      <body>
-        {/* `NextError` is the default Next.js error page component. Its type
-        definition requires a `statusCode` prop. However, since the App Router
-        does not expose status codes for errors, we simply pass 0 to render a
-        generic error message. */}
-        <NextError statusCode={0} />
-      </body>
-    </html>
-  );
+  return <NextError statusCode={0} />;
 }
