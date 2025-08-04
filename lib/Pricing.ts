2331e04d1765ae3ac91c0fe45fb0489ea78204c2
Omar Badawy <omar.badawy@hotmail.com>
2025-08-05 00:50:32 +0300

    fixed environment variable

--- lib/Pricing.ts
+++ lib/Pricing.ts
@@ -1,14 +1,14 @@
 export const plans = [
   {
-    link: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_ATHLETE_PLAN_LINK: '',
-    priceId: process.env.NODE_ENV === 'development' ? process.env.ATHLETE_PLAN_ID : '',
+    link: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_ATHLETE_PLAN_LINK_TEST: process.env.NEXT_PUBLIC_ATHLETE_PLAN_LINK,
+    priceId: process.env.NODE_ENV === 'development' ? process.env.ATHLETE_PLAN_ID_TEST : process.env.ATHLETE_PLAN_ID,
     price: 12.99,
     currency: 'eur',
     duration: '/month',
   },
   {
-    link: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_PAY_AS_YOU_GO_LINK : '',
-    priceId: process.env.NODE_ENV === 'development' ? process.env.PAY_AS_YOU_GO_ID : '',
+    link: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_PAY_AS_YOU_GO_LINK_TEST : process.env.NEXT_PUBLIC_PAY_AS_YOU_GO_LINK,
+    priceId: process.env.NODE_ENV === 'development' ? process.env.PAY_AS_YOU_GO_ID_TEST : process.env.PAY_AS_YOU_GO_ID,
     price: 2.99,
     currency: 'eur',
   },
